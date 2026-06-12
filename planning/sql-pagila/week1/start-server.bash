eea25904c7b196a4dbb446d15611245b5df91114
Haresh Bhuriya <hareshybhuriya@gmail.com>
2026-06-12 22:59:12 +0530

    start server in windows using command line (powershell)

---
+++ planning/sql-pagila/week1/start-server.bash
@@ -0,0 +1,5 @@
+# Start Server using Powershell in windows
+
+cd C:\postgres\postgresql-18.4-2-windows-x64-binaries\pgsql\bin
+
+C:\postgres\postgresql-18.4-2-windows-x64-binaries\pgsql\bin> .\pg_ctl.exe -D "C:\postgres\data" -l "C:\postgres\postgresql-18.4-2-windows-x64-binaries\pgsql\logfile.log" start
